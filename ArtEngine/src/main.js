8acac43a8bcead0702e222d702bdb01bd28f981c
ArielRin <degenftm@gmail.com>
2024-01-14 20:48:01 +1100

    metadata

--- ArtEngine/src/main.js
+++ ArtEngine/src/main.js
@@ -139,7 +139,7 @@ const addMetadata = (_dna, _edition) => {
     date: dateTime,
     ...extraMetadata,
     attributes: attributesList,
-    compiler: "HashLips Art Engine",
+    compiler: "Alpha7 Art Engine",
   };
   if (network == NETWORK.sol) {
     tempMetadata = {
